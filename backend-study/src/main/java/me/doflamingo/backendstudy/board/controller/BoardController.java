938f2c51c4fa841a07073c925b54ead23da6116c
Lee SangHyup <bosuksh@gmail.com>
2021-03-21 12:10:08 +0900

    feat: 게시글 리스트 조회

--- backend-study/src/main/java/me/doflamingo/backendstudy/board/controller/BoardController.java
+++ backend-study/src/main/java/me/doflamingo/backendstudy/board/controller/BoardController.java
@@ -5,25 +5,30 @@ import me.doflamingo.backendstudy.board.dto.PostRequestDto;
 import me.doflamingo.backendstudy.board.dto.PostResponseDto;
 import me.doflamingo.backendstudy.board.service.BoardService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/posts")
 public class BoardController {
 
   private final BoardService boardService;
 
-  @PostMapping("/posts")
+  @PostMapping
   public ResponseEntity<?> writePost(@Valid @RequestBody PostRequestDto requestDto) {
     PostResponseDto postResponseDto = boardService.writePost(requestDto);
     URI uri = URI.create("http://localhost:8080/posts/"+postResponseDto.getId());
     return ResponseEntity.created(uri).body(postResponseDto);
   }
+
+  @GetMapping
+  public ResponseEntity<?> getPostList() {
+    return ResponseEntity.ok(boardService.getPostList());
+  }
+
+
 
 }
